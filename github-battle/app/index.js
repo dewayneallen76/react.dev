03a25e6ba961075143500b67d5b3e0ceb357cf59
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-01 14:49:58 -0500

    added components folder, exported, and setup the require statements on index.js

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,22 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-
-/*
-Component has three things:
- - State
- - Lifecycle event
- - UI (must have the UI, state and lifecyle may not be needed. UI is what is being rendered)
- */
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello React Training!
-      </div>
-    )
-  }
-}
+var App = require('./components/App');
 
 ReactDOM.render(
   <App />,
