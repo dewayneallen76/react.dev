1e6804f90c376ae00591e5d21a4f710d54974129
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-06 13:58:01 -0500

    working on getting the notes to render on the board

--- notes/src/Board.js
+++ notes/src/Board.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Note from './App';
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: 'Call Bill',
+             'Email Bill',
+             'Kill Bill',
+             'Bury Bill'
+    }
+  }
   render() {
     return (
       <div className="board">
-        {this.props.count}
+        {this.state.notes.map((note, i) => {
+          return <Note key={i}>{note}</Note>
+        })}
       </div>
     )
   }
 }
 
 Board.propTypes = {
-  count: PropTypes.number.isRequired,
+  count: PropTypes.func.isRequired,
 };
 
 export default Board;
