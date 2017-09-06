af0c256327dbc335b1448f0ef5480aaf41beccc1
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-06 13:32:46 -0500

    updated index.js to render the board component

--- notes/src/index.js
+++ notes/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Board from './Board';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App>This is a note.</App>, document.getElementById('root'));
+ReactDOM.render(<Board count={10} />, document.getElementById('root'));
 registerServiceWorker();
