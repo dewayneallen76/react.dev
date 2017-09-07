6b5d1325e103c7353dfcd0f8898288702138b973
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-07 14:23:38 -0500

    changed id for getElementById to use 'react-container'

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Board from './Board';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Board count={50} />, document.getElementById('root'));
+ReactDOM.render(<Board count={25} />, document.getElementById('react-container'));
 registerServiceWorker();
