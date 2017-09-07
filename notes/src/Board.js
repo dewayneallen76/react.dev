97439c029244ba64899a00047bd9590716f6efe4
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-07 10:17:03 -0500

    added methods: update, remove, eachNote, to change the properties also refactored the render method to only use the eachNote method moving the map function to the eachNote method.

--- notes/src/Board.js
+++ notes/src/Board.js
@@ -2,24 +2,51 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Note from './App';
 
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       notes: [
-        'Call Bill',
-        'Email Bill',
-        'Kill Bill',
-        'Bury Bill'
+        {id: 0, note: 'Call Bill'},
+        {id: 1, note: 'Email Bill'},
+        {id: 2, note: 'Kill Bill'},
+        {id: 3, note: 'Bury Bill'}
       ]
     }
+    this.update = this.update.bind(this);
+    this.remove = this.remove.bind(this);
+    this.eachNote = this.eachNote.bind(this);
+  }
+  update(newText, id) {
+    var notes = this.state.notes.map(
+      note => (note.id !== id) ?
+        note:
+          {
+            note: newText
+          }
+    )
+    this.setState({notes})
+  }
+  remove(id) {
+    var notes = this.state.notes.filter(note => note.id !== id)
+    this.setState({notes})
+  }
+  eachNote(note) {
+    return (
+      <Note key={note.id}
+            id={note.id}
+            onChange={this.update}
+            onRemove={this.remove}>
+          {note.note}
+      </Note>
+    )
+
   }
   render() {
     return (
       <div className="board">
-        {this.state.notes.map((note, i) => {
-          return <Note key={i}>{note}</Note>
-        })}
+        {this.state.notes.map(this.eachNote)}
       </div>
     )
   }
