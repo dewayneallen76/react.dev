14406089f555ce9e5742c940215169b2ff594c94
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-08 13:42:39 -0500

    Added Player component, used it in the App js

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
+import Player from './Player';
 import PropTypes from 'prop-types';
 
 class App extends Component {
@@ -10,18 +11,7 @@ class App extends Component {
         <Header title={this.props.title} />
 
         <div className="players">
-          <div className="player">
-            <div className="player-name">
-              Dewayne
-            </div>
-            <div className="player-score">
-              <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <div className="counter-score"> 30 </div>
-                <button className="counter-action increment"> + </button>
-              </div>
-            </div>
-          </div>
+          <Player name="Dewayne" score={30} />
 
           <div className="player">
             <div className="player-name">
---
+++ scoreboard/src/Player.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Player extends Component {
+  render () {
+    return (
+      <div className="player">
+        <div className="player-name">
+          {this.props.name}
+        </div>
+        <div className="player-score">
+          <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <div className="counter-score"> {this.props.score} </div>
+            <button className="counter-action increment"> + </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Player.propTypes = {
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired
+}
+
+export default Player;
