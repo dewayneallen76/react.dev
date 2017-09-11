48fadb6a5d2cfb8bd612ee6054cadbe13ec53088
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 10:32:43 -0500

    added array of objects for players

--- scoreboard/src/index.js
+++ scoreboard/src/index.js
@@ -4,5 +4,28 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var PLAYERS = [
+  {
+    name: "Dewayne Allen",
+    score: 35,
+    id: 1,
+  },
+  {
+    name: "Kristy Allen",
+    score: 20,
+    id: 2,
+  },
+  {
+    name: "MaryAlice Holstead",
+    score: 15,
+    id: 3,
+  },
+  {
+    name: "Lola Allen",
+    score: 10,
+    id: 4,
+  },
+]
+
+ReactDOM.render(<App players={PLAYERS}/>, document.getElementById('root'));
 registerServiceWorker();
