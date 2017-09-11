69cf85173c12ff7ada57b92b994b7dee42e2b9bd
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 09:52:46 -0500

    changed to a stateless function

--- scoreboard/src/Header.js
+++ scoreboard/src/Header.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-class Header extends Component {
-  render () {
-    return (
-      <div className="header">
-        <h1>{this.props.title}</h1>
-      </div>
-    )
-  }
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  )
 }
 
 Header.propTypes = {
