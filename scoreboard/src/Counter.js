a104a2265e2a1a17b58724d37acc3c0fb8b237b7
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 13:44:54 -0500

    changed Counter back to a stateless functional component, so that state can be established later

--- scoreboard/src/Counter.js
+++ scoreboard/src/Counter.js
@@ -1,41 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      score: 0,
-      propTypes: {
-        initialScore: PropTypes.number.isRequired,
-      }
-
-    }
-  this.incrementScore = this.incrementScore.bind(this);
-  this.decrementScore = this.decrementScore.bind(this);
-  }
-
-  incrementScore() {
-    this.setState({
-      score: (this.state.score + 1),
-    })
-  }
-
-  decrementScore() {
-    this.setState({
-      score: (this.state.score -1),
-    })
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-        <div className="counter-score"> {this.state.score} </div>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
-      </div>
-    )
-  }
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
 }
+
+Counter.propTypes = {
+  score: PropTypes.number.isRequired,
+}
+
 
 export default Counter;
