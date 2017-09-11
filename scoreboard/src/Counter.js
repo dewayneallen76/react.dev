aff55937cea3a688b4f424508aedc451bbbe6f41
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 11:47:27 -0500

    changed Counter from a stateless functional component to a component class. Added state to set the initial value of the score to 0.

--- scoreboard/src/Counter.js
+++ scoreboard/src/Counter.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Counter(props) {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  )
-}
-
-Counter.propTypes = {
-  score: PropTypes.number.isRequired,
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+    }
+  }
+  propTypes: {
+    
+  }
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score"> {this.state.score} </div>
+        <button className="counter-action increment"> + </button>
+      </div>
+    )
+  }
 }
 
 export default Counter;
