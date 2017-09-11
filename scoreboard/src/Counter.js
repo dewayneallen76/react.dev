b571fd62cbfab672d40308569e8f67b0a7fe58be
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 14:22:10 -0500

    added onClick to call onChange property function

--- scoreboard/src/Counter.js
+++ scoreboard/src/Counter.js
@@ -4,15 +4,16 @@ import PropTypes from 'prop-types';
 function Counter(props) {
   return (
     <div className="counter">
-      <button className="counter-action decrement"> - </button>
+      <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
       <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
+      <button className="counter-action increment" onClick={function() {props.onChange(+1);}}> + </button>
     </div>
   )
 }
 
 Counter.propTypes = {
   score: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
 
