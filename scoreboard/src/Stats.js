df2f7546dba12e945b76e2fb689be6fd767fb899
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 17:05:30 -0500

    created Stats component

---
+++ scoreboard/src/Stats.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Stats(props) {
+
+  var totalPlayers = props.players.length;
+
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>123</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+Stats.propTypes = {
+  players: PropTypes.array.isRequired,
+}
+
+export default Stats;
