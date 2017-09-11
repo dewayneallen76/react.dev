14576c1ff72a48b00813239be1085718b9dba605
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 12:06:52 -0500

    bound methods incrementScore and decrementScore, added onClick for both buttons

--- scoreboard/src/Counter.js
+++ scoreboard/src/Counter.js
@@ -6,17 +6,33 @@ class Counter extends React.Component {
     super(props);
     this.state = {
       score: 0,
+
     }
+  this.incrementScore = this.incrementScore.bind(this);
+  this.decrementScore = this.decrementScore.bind(this);
   }
   propTypes: {
-    
+
   }
+
+  incrementScore() {
+    this.setState({
+      score: (this.state.score + 1),
+    })
+  }
+
+  decrementScore() {
+    this.setState({
+      score: (this.state.score -1),
+    })
+  }
+
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
         <div className="counter-score"> {this.state.score} </div>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     )
   }
