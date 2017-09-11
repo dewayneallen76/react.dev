1b9a7df9013186f543370fbfce390a92627ae908
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-11 10:00:31 -0500

    removed component from React import statement since these were changed from components to stateless functions.

--- scoreboard/src/Counter.js
+++ scoreboard/src/Counter.js
@@ -1,16 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Counter extends Component {
-  render () {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <div className="counter-score"> {this.props.score} </div>
-        <button className="counter-action increment"> + </button>
-      </div>
-    )
-  }
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
 }
 
 Counter.propTypes = {
