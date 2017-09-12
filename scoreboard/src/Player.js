056bb7fba485d4a505369c91566fad453bf4e4fc
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-12 11:10:12 -0500

    added onRemove function to propTypes

--- scoreboard/src/Player.js
+++ scoreboard/src/Player.js
@@ -7,6 +7,7 @@ function Player(props) {
   return (
     <div className="player">
       <div className="player-name">
+        <a className="remove-player" onClick={props.onRemove}>X</a>
         {props.name}
       </div>
       <div className="player-score">
@@ -20,6 +21,7 @@ Player.propTypes = {
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   onScoreChange: PropTypes.func.isRequired,
+  onRemove: PropTypes.func.isRequired,
 }
 
 export default Player;
