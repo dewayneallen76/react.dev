d9d1639eebb2ceb7f7f473d55fa939c3dc879a3d
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-12 14:15:44 -0500

    added methods to stopwatch component so that time could be started, stopped and reset.

--- scoreboard/src/Stopwatch.js
+++ scoreboard/src/Stopwatch.js
@@ -1,13 +1,60 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      running: false,
+      elapsedTime: 0,
+      previousTime: 0,
+    }
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+    this.onTick = this.onTick.bind(this);
+    this.onReset = this.onReset.bind(this);
+  }
+  componentDidMount() {
+    this.interval = setInterval(this.onTick, 100)
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  onTick() {
+    console.log("onTick");
+    if (this.state.running) {
+      var now = Date.now();
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime)
+      })
+    }
+  }
+  onStart() {
+    this.setState({
+      running: true,
+      previousTime: Date.now()})
+  }
+  onStop() {
+    this.setState({running: false})
+  }
+  onReset() {
+    this.setState({
+      elapsedTime: 0,
+      previousTime: Date.now(),
+    })
+  }
   render() {
+    var seconds = Math.floor(this.state.elapsedTime / 1000);
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <div className="stopwatch-time">0</div>
-        <button>Start</button>
-        <button>Reset</button>
+        <div className="stopwatch-time">{seconds}</div>
+        {this.state.running ?
+          <button onClick={this.onStop}>Stop</button>
+          :
+          <button onClick={this.onStart}>Start</button>
+        }
+        <button onClick={this.onReset}>Reset</button>
       </div>
     )
   }
