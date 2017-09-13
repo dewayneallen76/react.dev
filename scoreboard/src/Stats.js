2d65c9f1468b3b5536685fa3b8eab9a12eb918ef
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-12 21:51:38 -0500

    installed firebase so that I could deploy the app on firebase.

--- scoreboard/src/Stats.js
+++ scoreboard/src/Stats.js
@@ -3,21 +3,21 @@ import PropTypes from 'prop-types';
 
 
 function Stats(props) {
-  // var totalPlayers = props.players.length;
-  // var totalPoints = props.players.reduce(function(total, player){
-  //   return total + player.score;
-  // }, 0);
+  var totalPlayers = props.players.length;
+  var totalPoints = props.players.reduce(function(total, player){
+      return total + player.score;
+      }, 0);
 
   return (
     <table className="stats">
       <tbody>
         <tr>
           <td>Players:</td>
-          <td>4</td>
+          <td>{totalPlayers}</td>
         </tr>
         <tr>
           <td>Total Points:</td>
-          <td>123</td>
+          <td>{totalPoints}</td>
         </tr>
       </tbody>
     </table>
