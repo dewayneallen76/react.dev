4affe2f4af2277cd15bd37051c23a5e4882e09ad
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-13 15:06:11 -0500

    Added state to the App component, added method getTotalInvited, also created commented out methods that will need to be used later

--- rsvp/src/App.js
+++ rsvp/src/App.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// App components
+import GuestList from './GuestList';
+
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Treasure',
+        isConfirmed: false,
+      },
+      {
+        name: 'Nic',
+        isConfirmed: true,
+      }
+    ]
+  }
+
+  getTotalInvited = () => this.state.guests.length;
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
+
   render() {
     return (
       <div className="App">
@@ -36,32 +57,7 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li className="pending"><span>Safia</span></li>
-            <li className="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li className="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox" /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+          <GuestList />
         </div>
       </div>
     );
