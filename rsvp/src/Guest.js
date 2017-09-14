0eb4c8f81ea990977d24cf90b7d7b00265f20c67
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-14 10:34:19 -0500

    added propTypes for isEditing and handleToggleEditing, added onClick to the edit button to props.handleToggleEditing.

--- rsvp/src/Guest.js
+++ rsvp/src/Guest.js
@@ -12,7 +12,7 @@ const Guest = props => {
           onChange={props.handleConfirmation}
         /> Confirmed
       </label>
-      <button>edit</button>
+      <button onClick={props.handleToggleEditing}>edit</button>
       <button>remove</button>
     </li>
   )
@@ -21,7 +21,9 @@ const Guest = props => {
 Guest.propTypes = {
   name: PropTypes.string.isRequired,
   isConfirmed: PropTypes.bool.isRequired,
+  isEditing: PropTypes.bool.isRequired,
   handleConfirmation: PropTypes.func.isRequired,
+  handleToggleEditing: PropTypes.func.isRequired,
 }
 
 export default Guest;
