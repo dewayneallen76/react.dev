6bb49299148f9fe6be6be2c2d367b1dfbc3f288e
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-14 13:49:04 -0500

    added functionality to remove guest, passed it along through the components that will need it.

--- rsvp/src/Guest.js
+++ rsvp/src/Guest.js
@@ -21,7 +21,7 @@ const Guest = props => {
       <button onClick={props.handleToggleEditing}>
         {props.isEditing ? "save" : "edit"}
       </button>
-      <button>remove</button>
+      <button onClick={props.handleRemove}>remove</button>
     </li>
   )
 }
@@ -33,6 +33,7 @@ Guest.propTypes = {
   handleConfirmation: PropTypes.func.isRequired,
   handleToggleEditing: PropTypes.func.isRequired,
   setName: PropTypes.func.isRequired,
+  handleRemove: PropTypes.func.isRequired,
 }
 
 export default Guest;
