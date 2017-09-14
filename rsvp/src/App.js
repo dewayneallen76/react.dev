8a1301a900d287c8a5133f8f6b0c1b30171c2a7f
Dewayne Allen <dewayneallen76@gmail.com>
2017-09-14 10:29:06 -0500

    added isEditing state to the guests array, refactored toggleConfirmationAt to toggleGuestPropertyAt so that it can be used to toggle confirmation and is editing. added props to GuestList

--- rsvp/src/App.js
+++ rsvp/src/App.js
@@ -11,30 +11,39 @@ class App extends Component {
       {
         name: 'Treasure',
         isConfirmed: false,
+        isEditing: false,
       },
       {
         name: 'Nic',
         isConfirmed: true,
+        isEditing: false,
       },
       {
         name: 'Lola',
         isConfirmed: true,
+        isEditing: true,
       }
     ]
   }
 
-  toggleConfirmationAt = indexToChange =>
+  toggleGuestPropertyAt = (property, indexToChange) =>
     this.setState({
       guests: this.state.guests.map((guest, index) => {
         if (index === indexToChange) {
           return {
             ...guest,
-            isConfirmed: !guest.isConfirmed
+            [property]: !guest.[property]
           };
         }
         return guest;
       })
     });
+
+  toggleConfirmationAt = index =>
+    this.toggleGuestPropertyAt("isConfirmed", index);
+
+  toggleEditingAt = index =>
+    this.toggleGuestPropertyAt("isEditing", index);
 
   getTotalInvited = () => this.state.guests.length;
   // getAttendingGuests = () =>
@@ -76,6 +85,7 @@ class App extends Component {
           <GuestList
             guests={this.state.guests}
             toggleConfirmationAt={this.toggleConfirmationAt}
+            toggleEditingAt={this.toggleEditingAt}
           />
         </div>
       </div>
